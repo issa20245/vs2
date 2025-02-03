229b1672d2d30e14217f8dc371f8459d270a0fe9
issa20245 <166624768+issa20245@users.noreply.github.com>
2025-02-03 22:18:59 +0000

    Your commit message

---
+++ stacalist.py
@@ -0,0 +1,21 @@
+import math
+import random as r
+def log(*arg):
+    for i in arg:
+        print(arg)
+value = []
+def store(val):
+    value.append(val)
+def sqrt(opr):
+     return math.sqrt(opr)
+def big(*num):
+    for i in range(1):
+        print(max(num))
+def small(*num):
+    for i in range(1):
+        print(min(num))
+def rand(a,b):
+    return print(r.randint(a,b))
+def grf(h,w):
+    for i in range(h):
+        print(w)
